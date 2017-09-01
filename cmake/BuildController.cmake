2bc751475e1f8bacc183ab3e22af4f20ccb39e8a
Luca Muratore <luca.muratore@iit.it>
2017-09-01 20:30:55 +0200

    Controller added to the superbuild

---
+++ cmake/BuildController.cmake
@@ -0,0 +1,14 @@
+# Controller
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(XCM QUIET)
+
+ycm_ep_helper(  Controller
+                TYPE GIT
+                STYLE GITHUB
+                REPOSITORY ADVRHumanoids/Controller.git
+                TAG master
+                COMPONENT external
+		DEPENDS XCM
+             )
