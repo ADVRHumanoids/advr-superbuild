00c65b2f57d2f2f3c646bf2833b67197eabcf369
Luca Muratore <luca.muratore@iit.it>
2017-09-26 11:31:16 +0200

    XBotCore is free from low level libraries.

--- cmake/BuildXBotCore.cmake
+++ cmake/BuildXBotCore.cmake
@@ -4,20 +4,13 @@ include(FindOrBuildPackage)
 
 find_or_build_package(XCM QUIET)
 
-find_or_build_package(soem_advr QUIET)
-find_or_build_package(walkman-master-ecat QUIET)
-find_or_build_package(ec_master_test QUIET)
-
 ycm_ep_helper(  XBotCore
                 TYPE GIT
                 STYLE GITHUB
                 REPOSITORY ADVRHumanoids/xbotcore.git
-                TAG centauro_bonn_review_2016
+                TAG master
                 COMPONENT external
 		DEPENDS XCM
-			soem_advr
-			walkman-master-ecat
-			ec_master_test
 		CMAKE_CACHE_ARGS -DBUILD_SHARED_LIBS:BOOL=ON
                                  -DENABLE_XENO:BOOL=ON
 
