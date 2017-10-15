55cc4865838405a89b406ee429cbb62fe505b4fe
Luca Muratore <luca.muratore@iit.it>
2017-10-15 20:32:29 +0200

    Added Playback_mat_with_DK in the superbuild

---
+++ cmake/BuildPlayback_mat_with_DK.cmake
@@ -0,0 +1,14 @@
+# Playback_mat_with_DK
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(XCM QUIET)
+
+ycm_ep_helper(  Playback_mat_with_DK
+                TYPE GIT
+                STYLE GITHUB
+                REPOSITORY ADVRHumanoids/Playback_mat_with_DK.git
+                TAG master
+                COMPONENT external
+                DEPENDS XCM
+             )
