428380ad75be26a0495cfe155737120256535f48
Arturo Laurenzi <arturo.laurenzi@iit.it>
2017-11-03 12:57:57 +0100

    Added centauro_tools

---
+++ cmake/Buildnlopt.cmake
@@ -0,0 +1,12 @@
+# nlopt
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+
+ycm_ep_helper(  nlopt
+                TYPE GIT
+                STYLE GITHUB
+                REPOSITORY ADVRHumanoids/nlopt.git
+                TAG master
+                COMPONENT external
+            )
