d5d17c5fe706877e88d1ef4af2b8a2eb45e85787
Navvab Kashiri <navvab.kashiri@iit.it>
2017-11-16 22:28:54 +0100

    Repository address correction

--- cmake/BuildParking_Steering.cmake
+++ cmake/BuildParking_Steering.cmake
@@ -7,7 +7,7 @@ find_or_build_package(XCM QUIET)
 ycm_ep_helper(  Parking_Steering
                 TYPE GIT
                 STYLE GITHUB
-                REPOSITORY git@github.com:ADVRHumanoids/Parking_Steering.git
+                REPOSITORY ADVRHumanoids/Parking_Steering.git
                 TAG master
                 COMPONENT external
                 DEPENDS XCM)
