80b2cbf24458baedb94dbbd0c847230fb179dec3
Danilo Caporale <danilo.caporale@gmail.com>
2018-02-07 01:28:55 +0100

    Pushing update for new superbuild.

--- cmake/Buildlazarus.cmake
+++ cmake/Buildlazarus.cmake
@@ -2,16 +2,13 @@
 include(YCMEPHelper)
 include(FindOrBuildPackage)
 
-find_or_build_package(drc_shared QUIET)
-find_or_build_package(idynutils QUIET)
-find_or_build_package(GYM QUIET)
-find_or_build_package(trajectory_generator QUIET)
+find_or_build_package(XBotCore QUIET)
 
 ycm_ep_helper(  lazarus
                 TYPE GIT
                 STYLE GITHUB
                 REPOSITORY CentroEPiaggio/lazarus.git
                 TAG master
-                COMPONENT robots
-                DEPENDS drc_shared idynutils GYM XBotCoreModel XBotInterface RobotInterfaceYARP ModelInterfaceRBDL
+                COMPONENT external
+                DEPENDS XBotCore
             )
