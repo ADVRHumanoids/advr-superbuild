177360ff4fef4b4e8cccc33a14ef79e871909d76
pepperig <giusepperig@hotmail.it>
2018-06-07 11:36:44 +0200

    updating the new master libraries remote

--- cmake/Buildec_master_test.cmake
+++ cmake/Buildec_master_test.cmake
@@ -3,16 +3,16 @@ include(YCMEPHelper)
 include(FindOrBuildPackage)
 
 find_or_build_package(soem_advr QUIET)
-find_or_build_package(walkman-master-ecat QUIET)
+find_or_build_package(ecat_master_advr QUIET)
 
 ycm_ep_helper(  ec_master_test
                 TYPE GIT
-                STYLE GITLAB_ADVRCLOUD
-                REPOSITORY amargan/ec_master_test.git
+                STYLE GITLAB_ADVR
+                REPOSITORY xeno-ecat/ec_master_test.git
                 TAG xeno-3
                 COMPONENT external
 		DEPENDS soem_advr
-			ecat-master-advr
+			ecat_master_advr
 		CMAKE_CACHE_ARGS -DBUILD_SHARED_LIBS:BOOL=ON
                                  -DENABLE_XENO:BOOL=ON
 
