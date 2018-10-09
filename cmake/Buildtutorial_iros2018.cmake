230ed291fb831f6e9edf062f7fddd3fab8eb1fe9
EnricoMingo <enricomingo@gmail.com>
2018-10-09 16:05:31 +0200

    Added tutorial in superbuild

---
+++ cmake/Buildtutorial_iros2018.cmake
@@ -0,0 +1,10 @@
+# tutorial_iros2018
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+ycm_ep_helper(  tutorial_iros2018 TYPE GIT
+                STYLE GITHUB
+                REPOSITORY ADVRHumanoids/tutorial_iros2018.git
+                TAG dev
+                COMPONENT external
+)
