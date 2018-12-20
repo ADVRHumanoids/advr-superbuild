8a8bd2dd8dc9f93f43c9256fbdd848e004708dd2
EnricoMingo <enricomingo@gmail.com>
2018-12-20 11:45:20 +0100

    fix

--- robotology-setup.bash
+++ robotology-setup.bash
@@ -139,6 +139,7 @@ pathadd ROS_PACKAGE_PATH $ROBOTOLOGY_ROOT/robots/hyqreal-description
 pathadd ROS_PACKAGE_PATH $ROBOTOLOGY_ROOT/robots/iit-teleop-ros-pkg
 pathadd ROS_PACKAGE_PATH $ROBOTOLOGY_ROOT/robots/iit-hyqreal-teleop-ros-pkg
 pathadd GAZEBO_MODEL_PATH $ROBOTOLOGY_ROOT/robots/iit-teleop-ros-pkg/teleop_gazebo/database
+pathadd GAZEBO_MODEL_PATH $ROBOTOLOGY_ROOT/robots/iit-hyqreal-teleop-ros-pkg/hyqreal-teleop_gazebo/database
 pathadd CPATH $ROBOTOLOGY_ROOT/build/install/include
 pathadd CPATH /opt/ros/${ROS_DISTRO}/include
 pathadd PYTHONPATH $ROBOTOLOGY_ROOT/build/install/lib/python2.7/site-packages
