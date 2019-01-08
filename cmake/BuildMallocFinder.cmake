faadad8063e51182cc02dbffa44d1a0180b2aa39
EnricoMingo <enricomingo@gmail.com>
2019-01-08 16:00:30 +0100

    Added MallocFinder to superbuild

---
+++ cmake/BuildMallocFinder.cmake
@@ -0,0 +1,11 @@
+# MallocFinder
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+ycm_ep_helper(MallocFinder
+              TYPE GIT
+              STYLE GITHUB
+              REPOSITORY ADVRHumanoids/MallocFinder.git
+              TAG master
+              COMPONENT external
+             )
