05c6a45d7256bceb322ba8d9d5e7ccdfc4e85714
Luca Muratore <luca.muratore@iit.it>
2019-04-08 14:10:23 +0200

    updating TAG of rbdl since v2.6.0 contains important bugs

--- cmake/Buildrbdl.cmake
+++ cmake/Buildrbdl.cmake
@@ -6,7 +6,7 @@ ycm_ep_helper(  rbdl
                 TYPE HG
                 STYLE BITBUCKET
                 REPOSITORY rbdl/rbdl
-                TAG v2.6.0
+                TAG 338e4db
 		CMAKE_CACHE_ARGS -DRBDL_BUILD_ADDON_URDFREADER:BOOL=ON
                 COMPONENT external
             )
