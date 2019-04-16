32f762f8c6498e49bea3e00900f3c4715b6a38e8
Luca Muratore <luca.muratore@iit.it>
2019-04-16 13:14:14 +0200

    fixing https://github.com/ADVRHumanoids/advr-superbuild/issues/33

--- cmake/Buildrbdl.cmake
+++ cmake/Buildrbdl.cmake
@@ -6,7 +6,7 @@ ycm_ep_helper(  rbdl
                 TYPE HG
                 STYLE BITBUCKET
                 REPOSITORY rbdl/rbdl
-                TAG 338e4db
+                TAG v2.5.0
 		CMAKE_CACHE_ARGS -DRBDL_BUILD_ADDON_URDFREADER:BOOL=ON
                 COMPONENT external
             )
